2240c57276e1c21ec5d39a391c99adc08646e5b1
Adamlotfy <161876835+Adamloty@users.noreply.github.com>
2024-11-15 16:45:41 +0200

    انا جامد

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Services from '@/components/Services'
@@ -15,7 +14,6 @@ const FloatingButton = dynamic(() => import('../components/FloatingButton'), {
 export default function Home() {
  return (
  <>
-  <Header />
   <main>
    <FloatingButton />
    <section id="home">
